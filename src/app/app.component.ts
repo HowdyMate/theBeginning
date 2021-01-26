735e9b3c1981ffffe9da68b12c2fae83385ab13c
Pradeep2898 <spanand2898@gmail.com>
2021-01-27 02:08:58 +0530

    lot of changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Beginning';
+  ngOnInit() {
+    $(".btn-three").click(function () {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $('.second').offset().top
+      }, 1500);
+    });
+  }
 }
