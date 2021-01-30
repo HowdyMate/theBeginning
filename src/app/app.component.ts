e572cf8911d6b555f78369882c898b524a9aa254
Pradeep2898 <spanand2898@gmail.com>
2021-01-30 13:04:40 +0530

    added jquery scrollTop() for all navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Beginning';
   ngOnInit() {
-    $(".btn-three").click(function () {
+    $(".btn-three").on("click",() => {
       $([document.documentElement, document.body]).animate({
-        scrollTop: $('.second').offset().top
-      }, 1500);
+        scrollTop: $('.second').offset().top - 50
+      }, 100);
+    });
+    $(".nav-item a:contains('about')").on("click",() => {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $('.second').offset().top - 50
+      }, 100);
+    });
+    $(".nav-item a:contains('home')").on("click",() => {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $('.top').offset().top - 50
+      }, 100);
+    });
+    $(".nav-item a:contains('services')").on("click",() => {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $('.third').offset().top - 50
+      }, 100);
+    });
+    $(".nav-item a:contains('works')").on("click",() => {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $('.fourth').offset().top - 50
+      }, 100);
+    });
+    $(".nav-item a:contains('contacts')").on("click",() => {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $('.fifth').offset().top - 50
+      }, 100);
+    });
+    $("ul.navbar-nav li").on("click",()=>{
+      $("ul.navbar-nav li a").removeClass("active");
+      $(this).addClass("active");
     });
   }
 }
